917991b755e19f6c8392bd74567c0820175c1f90
William Hudson <williamdhudson@gmail.com>
2019-07-30 22:53:47 -0400

    completed the first 3 types

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  var front = 0;
+  var back = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,23 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[front] = value;
+    front++
   };
 
   someInstance.dequeue = function() {
+    if (front - back > 0) {
+      var output = storage[back]
+      delete storage[back]
+      back++
+    } else {
+      size = 0
+    }
+    return output
   };
 
   someInstance.size = function() {
+    return front - back;
   };
 
   return someInstance;
