b5cf6c0b1784a5f2a2e1a734cf05198590925fb4
William Hudson <williamdhudson@gmail.com>
2019-07-31 18:46:56 -0400

    completed es6 stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,26 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(front, back, storage) {
+  	this.front = 0;
+  	this.back = 0;
+  	this.storage = {};
   }
-
+  enqueue(value) {
+  	this.storage[this.front] = value;
+  	this.front++;
+  }
+  dequeue() {
+  	if (this.front - this.back > 0) {
+  		var output = this.storage[this.back];
+  		delete this.storage[this.back]
+  		this.back++
+  	} else {
+  		var size = 0;
+  	}
+  	return output
+  }
+  size() {
+  	return this.front - this.back;
+  }
 }
