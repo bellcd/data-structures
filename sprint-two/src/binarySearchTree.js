fdb1eb2904936215521dd18a229d2e260018f419
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-08-03 17:09:51 -0400

    [working on binarySearchTree]

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,31 @@
 var BinarySearchTree = function(value) {
+  let node = {};
+  node.value = value;
+  // think both of these values should start at null ??
+  node.left = left;
+  node.right = right;
+
+  return node;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  // determine if value is greater than or less than current node's value
+  let position;
+  if (value < position) {
+    position = 'left';
+  } else {
+    position = 'right';
+  }
+  // create position to store result of left or right from above
+  // check if current node[position] exists or not
+  if (this[position]) {
+    // YES, recurse call
+    this[position].insert(value);
+  } else {
+    // NO, set value at node[position]
+    this[position] = BinarySearchTree(value);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
