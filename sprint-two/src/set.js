ea971dafdf3a05b07cc11590d300b12f0df073fd
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-08-03 15:10:14 -0400

    Implements set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  let location = this._storage.indexOf(item);
+  this._storage.splice(location, 1);
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? O(n)
  */
